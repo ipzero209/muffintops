1e372912683a2de08dfc9099b2b6917eb8421a3e
mohitbhasin1 <mohit_369@hotmail.com>
2020-08-13 16:16:52 -0700

    1.1.2

--- Muffin Tops/createDocument.js
+++
@@ -1,9 +0,0 @@
-function createDoc() {
-    var doc = DocumentApp.create('Sample Document');
-    var body = doc.getBody();
-    var rowsData = [['Plants', 'Animals'], ['Ficus', 'Goat'], ['Basil', 'Cat'], ['Moss', 'Frog']];
-    body.insertParagraph(0, doc.getName())
-        .setHeading(DocumentApp.ParagraphHeading.HEADING1);
-    table = body.appendTable(rowsData);
-    table.getRow(0).editAsText().setBold(true);
-  }
--- Muffin Tops/popup.js
+++ Muffin Tops/popup.js
@@ -12,4 +12,14 @@ let changeColor = document.getElementById('changeColor');
           tabs[0].id,
           {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
+    
+    function createDoc() {
+      var doc = DocumentApp.create('Sample Document');
+      var body = doc.getBody();
+      var rowsData = [['Plants', 'Animals'], ['Ficus', 'Goat'], ['Basil', 'Cat'], ['Moss', 'Frog']];
+      body.insertParagraph(0, doc.getName())
+          .setHeading(DocumentApp.ParagraphHeading.HEADING1);
+      table = body.appendTable(rowsData);
+      table.getRow(0).editAsText().setBold(true);
+    }
   };
